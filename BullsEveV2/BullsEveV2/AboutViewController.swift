f6b3facfca0e969fa35221d697ec588303b62bba
PoofyPow <37036682+PoofyPow@users.noreply.github.com>
2020-01-19 21:54:28 -0500

    Add files via upload

---
+++ BullsEveV2/BullsEveV2/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEveV2
+//
+//  Created by rahul on 2019-12-24.
+//  Copyright © 2019 rahul. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
